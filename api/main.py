24e71af19613ea0038a3bb938a3f365c4e7701d1
seccaz <1200847@isep.ipp.pt>
2025-10-16 10:40:41 +0100

    1.23 frontend url change for deployment

--- api/main.py
+++ api/main.py
@@ -46,11 +46,12 @@ app = FastAPI(
 
 # Configure CORS
 # Allow frontend URL from environment variable
-frontend_url = os.getenv("FRONTEND_URL", "http://localhost:5173")
+frontend_url = os.getenv("FRONTEND_URL", "https://perfumapi-frontend.onrender.com/")
 allowed_origins = [
     frontend_url,
     "http://localhost:5173",  # Local development
-    "http://localhost:3000",  # Alternative local port
+    "http://localhost:3000", 
+    "https://perfumapi-frontend.onrender.com/" # Alternative local port
 ]
 
 app.add_middleware(
